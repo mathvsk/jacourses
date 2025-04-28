0c8adbb02dc7078c8c6b6a4ed274243f2bba0ee9
Matheus Viscki <matheusviscki@gmail.com>
2025-04-28 20:49:47 -0300

    commit inicial

---
+++ src/main/java/com/matheus/jatasks/JatasksApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.jatasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JatasksApplication.class, args);
+    }
+
+}
