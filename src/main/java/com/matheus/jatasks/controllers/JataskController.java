a93c65472193498c1b4c3326737be260a430eb31
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 20:49:51 -0300

    feat: implementar useCase na rota post

--- src/main/java/com/matheus/jatasks/controllers/JataskController.java
+++ src/main/java/com/matheus/jatasks/controllers/JataskController.java
@@ -1,7 +1,11 @@
 package com.matheus.jatasks.controllers;
 
-import com.matheus.jatasks.dtos.PostTaskRequestDTO;
+import com.matheus.jatasks.dtos.CreateTaskDTO;
+import com.matheus.jatasks.services.CreateTaskUseCase;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/jatask")
 public class JataskController {
+
+    @Autowired
+    private CreateTaskUseCase createTaskUseCase;
+
     @PostMapping("/courses")
-    public void create(@Valid @RequestBody PostTaskRequestDTO postTaskRequestDTO) {
-        System.out.println("Creating task: " + postTaskRequestDTO);
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
+        this.createTaskUseCase.execute(createTaskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
