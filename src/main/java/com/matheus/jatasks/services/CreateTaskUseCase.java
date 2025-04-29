fa5889ad7973f5bb6c4d0a49142e263a7f669c15
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 20:49:37 -0300

    feat: criar CreateTaskUseCase

---
+++ src/main/java/com/matheus/jatasks/services/CreateTaskUseCase.java
@@ -0,0 +1,24 @@
+package com.matheus.jatasks.services;
+
+import com.matheus.jatasks.dtos.CreateTaskDTO;
+import com.matheus.jatasks.models.TaskModel;
+import com.matheus.jatasks.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateTaskUseCase {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public void execute(CreateTaskDTO createTaskDTO) {
+        var taskModel = TaskModel.builder()
+                .name(createTaskDTO.getName())
+                .category(createTaskDTO.getCategory())
+                .active(createTaskDTO.isActive())
+                .build();
+
+        this.taskRepository.save(taskModel);
+    }
+}
