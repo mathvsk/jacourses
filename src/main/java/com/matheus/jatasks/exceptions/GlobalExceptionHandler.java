e6a5231ccef8a5e3932cf88fb3576a22444d24f0
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 08:10:31 -0300

    feat: adicionar handler para tratativa MethodArgumentNotValidException

--- src/main/java/com/matheus/jatasks/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/matheus/jatasks/exceptions/GlobalExceptionHandler.java
@@ -13,6 +13,16 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(err ->
+            errors.put(err.getField(), err.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
         Map<String, String> body = new HashMap<>();
