b13bdda8f860d567ce25758bbdfc501443b68de2
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 08:13:09 -0300

    feat: criar DTO para requisição post

---
+++ src/main/java/com/matheus/jatasks/dtos/PostTaskRequestDTO.java
@@ -0,0 +1,21 @@
+package com.matheus.jatasks.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostTaskRequestDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String category;
+    private boolean active;
+}
