5702d4269c0f8ce16d755636305a139a8edfb7db
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 21:17:02 -0300

    feat: criar rota getAll

--- src/main/java/com/matheus/jatasks/controllers/JataskController.java
+++ src/main/java/com/matheus/jatasks/controllers/JataskController.java
@@ -1,15 +1,16 @@
 package com.matheus.jatasks.controllers;
 
 import com.matheus.jatasks.dtos.CreateTaskDTO;
-import com.matheus.jatasks.services.CreateTaskUseCase;
+import com.matheus.jatasks.dtos.GetAllTasksDTO;
+import com.matheus.jatasks.useCases.CreateTaskUseCase;
+import com.matheus.jatasks.useCases.GetAllTasksUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/jatask")
@@ -17,6 +18,20 @@ public class JataskController {
 
     @Autowired
     private CreateTaskUseCase createTaskUseCase;
+
+    @Autowired
+    private GetAllTasksUseCase getAllTasksUseCase;
+
+    @GetMapping("/courses")
+    public ResponseEntity<List<GetAllTasksDTO>> getAll() {
+        var result = this.getAllTasksUseCase.execute();
+
+        if (result.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 
     @PostMapping("/courses")
     public ResponseEntity<Object> create(@Valid @RequestBody CreateTaskDTO createTaskDTO) {
