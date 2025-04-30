eb7736f453f52290a1061e66c8f1c4b2e54a0a97
Matheus Viscki <matheusviscki@gmail.com>
2025-04-29 21:43:13 -0300

    feat: criar UpdateTaskUseCase

---
+++ src/main/java/com/matheus/jatasks/useCases/UpdateTaskUseCase.java
@@ -0,0 +1,24 @@
+package com.matheus.jatasks.useCases;
+
+import com.matheus.jatasks.dtos.UpdateTaskDTO;
+import com.matheus.jatasks.exceptions.NotFounException;
+import com.matheus.jatasks.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UpdateTaskUseCase {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public void execute(String id, UpdateTaskDTO updateTaskDTO) throws NotFounException {
+        var task = taskRepository.findById(UUID.fromString(id)).orElseThrow(() -> new NotFounException("Task not found"));
+        task.setName(updateTaskDTO.getName());
+        task.setCategory(updateTaskDTO.getCategory());
+
+        taskRepository.save(task);
+    }
+}
